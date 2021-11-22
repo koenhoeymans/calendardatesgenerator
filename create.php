80c71e96147a0fbd348fe252b578dd51ed9c2594
koenhoeymans <koenhoeymans@gmail.com>
2021-11-22 13:33:43 +0100

    update

--- create.php
+++ create.php
@@ -1,11 +1,17 @@
 <?php
 
+if (!isset($_GET['year'])) {
+    return;
+}
+
+$year = (int) $_GET['year'];
+$year = substr($year, 0, 4);
+
 $period = new DatePeriod(
-    new DateTime('2021-12-01'),
+    new DateTime($year . '-01-01 00:00:00'),
     new DateInterval('P1D'),
-    new DateTime('2022-12-31')
+    new DateTime($year . '-12-31 23:59:59')
 );
 
 foreach ($period as $key => $value) {
-    echo $value->format('Y-m-d D') . "\n";
-}
+    echo $value->format('Y-m-d D') . "<br />";
